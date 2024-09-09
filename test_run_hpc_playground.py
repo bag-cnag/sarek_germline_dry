e9580d6ce2dda7d0ca342cb696cda8f0caa06c5b
Davide Piscia <dpiscia@C02DL22UML7L.crg.es>
2024-09-09 07:42:08 +0200

    3rd condition

--- test_run_hpc_playground.py
+++ test_run_hpc_playground.py
@@ -35,7 +35,58 @@ dar_data1 = {"analysis_type":"germline",
  "tumor_experiment_id":"",
  "control_experiment_id":"HG002"}
 
+pipeline_def={
+      "analysis_type": "germline",
+      "name": "dry_germline",
+      "data": {
+        "repo": "bag-cnag/sarek_germline_dry",
+        "step": "mapping",
+        "steps": [
+          {
+            "step": "File availability and integrity",
+            "step_type": "process",
+            "description": ""
+          },
+          {
+            "step": "Sequencing data quality check",
+            "step_type": "qc",
+            "description": ""
+          },
+          {
+            "step": "Mapping to the reference genome",
+            "step_type": "process",
+            "description": ""
+          },
+          {
+            "step": "Mapping quality check",
+            "step_type": "qc",
+            "description": ""
+          },
+          {
+            "step": "Variant calling (SNV, CNV/SV) + Biomarkers/Pharmacogenomics",
+            "step_type": "process",
+            "description": ""
+          },
+          {
+            "step": "Variant calling quality check",
+            "step_type": "qc",
+            "description": ""
+          },
+          {
+            "step": "Pipeline completion check",
+            "step_type": "process",
+            "description": ""
+          }
+        ],
+        "tools": "cnvkit,manta,haplotypecaller,strelka,expansionhunter,stripy,fullmetrics",
+        "release": "v0.1",
+        "pipelines": "sarek,pcgx,annotatesvs,gatk_mt,qualitycontrols"
+      },
+    }
 
+def put_pipeline():
+    #only run whn needed we can not delete it via API
+    resp=requests.put(api_host+"/pipelines/",json=pipeline_def, headers=headers)
 
 def test_create_dar():
     resp=requests.put(api_host+"/dars/",json=dar_data1, headers=headers)
@@ -48,8 +99,11 @@ def test_run_task():
     status_array=resp.json()['data'][0]
     if len(status_array[1])==7:
         pytest.task_id=status_array[1]
-    else :
+    elif len(status_array[2])==7:
         pytest.task_id=status_array[2]
+        
+    else :
+        pytest.task_id=status_array[0]
     print(pytest.task_id)
     assert resp.status_code == 200
 
@@ -61,6 +115,7 @@ def test_run_execution():
         time.sleep(3)
         
         resp= requests.get(f"{api_host}/tasks/{pytest.task_id}", headers=headers)
+        
         status=resp.json()['data'][0]['status']
         if status=='completed':
             assert 1==1
